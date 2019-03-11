dc63b16b2cbb480e65a3e93a9efa02911a78eb61
Gabriel R. Abreu <grsabreu@gmail.com>
2019-03-11 17:02:58 -0300

    chore(component): export from Demo

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import {Text, requireNativeComponent} from 'react-native';
-
-const RNSelectableText = requireNativeComponent('RNSelectableText');
-
-export const SelectableText = ({ onSelection, ...props }) => {
-  const onSelectionNative = ({ nativeEvent: { content, eventType } })  => {
-    onSelection && onSelection({ content, eventType })
-  }
-
-  return (
-    <RNSelectableText {...props} onSelection={onSelectionNative} />
-  )
-}
+export { SelectableText } from './Demo/SelectableText'
