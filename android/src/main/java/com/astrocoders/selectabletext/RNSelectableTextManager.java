2b71e45b46035879358a833cb8836b7917719244
Gabriel R. Abreu <grsabreu@gmail.com>
2019-03-11 16:09:02 -0300

    feat(class): better instance management

--- android/src/main/java/com/astrocoders/selectabletext/RNSelectableTextManager.java
+++ android/src/main/java/com/astrocoders/selectabletext/RNSelectableTextManager.java
@@ -1,111 +1,118 @@
 package com.astrocoders.selectabletext;
 
-import com.facebook.react.uimanager.SimpleViewManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ActionMode;
+import android.view.ActionMode.Callback;
+
+import java.util.Map;
+
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.common.MapBuilder;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 import com.facebook.react.uimanager.annotations.ReactProp;
-import com.facebook.react.uimanager.ReactTextView;
+import com.facebook.react.views.text.ReactTextView;
+import com.facebook.react.views.text.ReactTextViewManager;
+
+import java.util.List;
+import java.util.ArrayList;
 
 
-public class RNSelectableTextManager extends SimpleViewManager<ReactTextView> {
-  public static final String REACT_CLASS = "RNSelectableText";
+public class RNSelectableTextManager extends ReactTextViewManager {
+    public static final String REACT_CLASS = "RNSelectableText";
 
-  String[] menuItems;
 
-  @Override
-  public String getName() {
-    return REACT_CLASS;
-  }
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
 
-  @Overrid
-  public ReactTextView createViewInstance(ThemedReactContext context){
-    return new ReactTextView(context);
-  }
+    @Override
+    public ReactTextView createViewInstance(ThemedReactContext context) {
+        return new ReactTextView(context);
+    }
 
-  public RNSelectableTextManager(Context context) {
-    super(context);
 
-    registerSelectionListener(view)
-  }
-
-  @ReactProp(name = "menuItems")
-  public void setMenuItems(ReactTextView textView, String[] items) {
-    menuItems = items;
-  }
-
-  public void registerSelectionListener(ReactTextView view) {
-    view.setCustomSelectionActionModeCallback(new Callback() {
-        @Override
-        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            menu.clear();
-            return true;
+    @ReactProp(name = "menuItems")
+    public void setMenuItems(ReactTextView textView, ReadableArray items) {
+        List<String> result = new ArrayList<String>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            result.add(items.getString(i));
         }
 
-        @Override
-        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-            // Called when action mode is first created. The menu supplied
-            // will be used to generate action buttons for the action mode
+        registerSelectionListener(result.toArray(new String[items.size()]), textView);
+    }
 
-            for (int i=0; i<menuItems.length; i++) { 
-              menu.add(0, i, 0, menuItems[i]);
+    public void registerSelectionListener(final String[] menuItems, final ReactTextView view) {
+        view.setCustomSelectionActionModeCallback(new Callback() {
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                // Called when action mode is first created. The menu supplied
+                // will be used to generate action buttons for the action mode
+                menu.removeItem(android.R.id.copy);
+                menu.removeItem(android.R.id.shareText);
+                menu.removeItem(android.R.id.selectAll);
+
+
+                return true;
             }
 
-            return true;
-        }
-
-        @Override
-        public void onDestroyActionMode(ActionMode mode) {
-            // Called when an action mode is about to be exited and
-        }
-
-        @Override
-        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-
-            int min = 0;
-            int max = view.getText().length();
-            if (view.isFocused()) {
-              // TODO: yield selection positions back to user also
-              final int selStart = view.getSelectionStart();
-              final int selEnd = view.getSelectionEnd();
-
-              min = Math.max(0, Math.min(selStart, selEnd));
-              max = Math.max(0, Math.max(selStart, selEnd));
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                for (int i = 0; i < menuItems.length; i++) {
+                    menu.add(0, i, 0, menuItems[i]);
+                }
+                return true;
             }
-            // Perform your definition lookup with the selected text
-            final CharSequence selectedText = view.getText().subSequence(min, max);
 
-            // Finish and close the ActionMode
-            mode.finish();
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                // Called when an action mode is about to be exited and
+            }
 
-            // Dispatch event
-            onSelectNativeEvent(menuItems[item.getItemId()], selectedText);
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                int selectionStart = view.getSelectionStart();
+                int selectionEnd = view.getSelectionEnd();
+                String selectedText = view.getText().toString().substring(selectionStart, selectionEnd);
 
-            return true;
-            return false;
-        }
+                // Dispatch event
+                onSelectNativeEvent(view, menuItems[item.getItemId()], selectedText);
 
-    });
-  }
+                mode.finish();
 
-  public void onSelectNativeEvent(String eventType, String content) {
-    WritableMap event = Arguments.createMap();
-    event.putString("eventType", eventType);
-    event.putString("content", content);
+                return true;
+            }
 
-    // Dispatch
-    ReactContext reactContext = (ReactContext)getContext();
-    reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
-      getId(),
-      "topChange",
-      event
-    );
-  }
+        });
+    }
 
-  @Override
-  public Map getExportedCustomDirectEventTypeConstants() {
-    return MapBuilder.of(
-        "topChange",
-        MapBuilder.of("onSelection"));
-  }
+    public void onSelectNativeEvent(ReactTextView view, String eventType, String content) {
+        WritableMap event = Arguments.createMap();
+        event.putString("eventType", eventType);
+        event.putString("content", content);
+
+        // Dispatch
+        ReactContext reactContext = (ReactContext) view.getContext();
+        reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                view.getId(),
+                "topSelection",
+                event
+        );
+    }
+
+    @Override
+    public Map getExportedCustomDirectEventTypeConstants() {
+        return MapBuilder.builder()
+                .put(
+                        "topSelection",
+                        MapBuilder.of(
+                                "registrationName","onSelection"))
+                .build();
+    }
 }
--- android/src/main/java/com/astrocoders/selectabletext/RNSelectableTextPackage.java
+++ android/src/main/java/com/astrocoders/selectabletext/RNSelectableTextPackage.java
@@ -1,4 +1,5 @@
 
+
 package com.astrocoders.selectabletext;
 
 import java.util.Arrays;
@@ -9,20 +10,16 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 public class RNSelectableTextPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNSelectableTextModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+      return Arrays.<ViewManager>asList(
+              new RNSelectableTextManager()
+      );
     }
 }
