2dfdd1f3bfa031e0993c9e4ece965e3dbd65ed28
George Lima <georgelima11@hotmail.com>
2019-03-19 19:53:27 -0300

    fix: issue with multiple instances and onSelection method

--- ios/RNSelectableTextView.m
+++ ios/RNSelectableTextView.m
@@ -12,53 +12,75 @@
     NSString* selectedText;
 }
 
--(void)setMenuItems:(NSArray<NSString *> *)menuItems {
-    UIMenuController *menuController = [UIMenuController sharedMenuController];
-    NSMutableArray *menuControllerItems = [NSMutableArray arrayWithCapacity:menuItems.count];
-    
-    for(NSString *menuItemName in menuItems) {
-        UIMenuItem *item = [[UIMenuItem alloc] initWithTitle: menuItemName
-                                                      action: @selector(handleMenuItemPress:)];
-        [menuControllerItems addObject: item];
-    }
-    
-    [menuController setMenuItems: menuControllerItems];
+- (dispatch_queue_t) methodQueue
+{
+    return dispatch_get_main_queue();
 }
 
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender
 {
-    return action == @selector(handleMenuItemPress:);
+    
+    NSString *sel = NSStringFromSelector(action);
+    NSRange match = [sel rangeOfString:@"_CUSTOM_SELECTOR_"];
+    if (match.location == 0) {
+        return YES;
+    }
+    return NO;
 }
 
 - (void)handleLongPress:(UILongPressGestureRecognizer *)gesture
 {
-    UIMenuController *menuController = [UIMenuController sharedMenuController];
-    
-    if (menuController.isMenuVisible) {
-        return;
-    }
-    
     if (!self.isFirstResponder) {
         [self becomeFirstResponder];
     }
     
+    UIMenuController *menuController = [UIMenuController sharedMenuController];
+    
+    if (menuController.isMenuVisible) return;
+    
+    NSMutableArray *menuControllerItems = [NSMutableArray arrayWithCapacity:self.menuItems.count];
+    
+    for(NSString *menuItemName in self.menuItems) {
+        NSString *sel = [NSString stringWithFormat:@"_CUSTOM_SELECTOR_%@", menuItemName];
+        UIMenuItem *item = [[UIMenuItem alloc] initWithTitle: menuItemName
+                                                      action: NSSelectorFromString(sel)];
+        
+        [menuControllerItems addObject: item];
+    }
+    
+    menuController.menuItems = menuControllerItems;
     [menuController setTargetRect:self.bounds inView:self];
     [menuController setMenuVisible:YES animated:YES];
-    
-    [self setMenuItems:self.menuItems];
-}
-
-
-- (void)handleMenuItemPress:(id) sender
-{
-    self.onSelection(@{
-        @"content": @("foo"),
-        @"eventType": @("foobar")
-    });
 }
 
 - (BOOL)canBecomeFirstResponder {
     return YES;
+}
+
+- (void)tappedMenuItem:(NSString *)text {
+    self.onSelection(@{
+                       @"content": text,
+                       @"eventType": text
+                       });
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)sel
+{
+    if ([super methodSignatureForSelector:sel]) {
+        return [super methodSignatureForSelector:sel];
+    }
+    return [super methodSignatureForSelector:@selector(tappedMenuItem:)];
+}
+
+- (void)forwardInvocation:(NSInvocation *)invocation
+{
+    NSString *sel = NSStringFromSelector([invocation selector]);
+    NSRange match = [sel rangeOfString:@"_CUSTOM_SELECTOR_"];
+    if (match.location == 0) {
+        [self tappedMenuItem:[sel substringFromIndex:17]];
+    } else {
+        [super forwardInvocation:invocation];
+    }
 }
 
 @end
