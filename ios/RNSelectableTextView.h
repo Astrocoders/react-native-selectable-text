24b1c1f1efaa212b19e01383d401ba6c0fcaa192
George Lima <georgelima11@hotmail.com>
2019-03-22 16:03:37 -0300

    chore(ios): set value prop to nonnull

--- ios/RNSelectableTextView.h
+++ ios/RNSelectableTextView.h
@@ -4,7 +4,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RNSelectableTextView : RCTBaseTextInputView
 
-@property (nullable, nonatomic, copy) NSString *value;
+@property (nonnull, nonatomic, copy) NSString *value;
 @property (nonatomic, copy) RCTDirectEventBlock onSelection;
 @property (nullable, nonatomic, copy) NSArray<NSString *> *menuItems;
 
