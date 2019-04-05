970a50a0999a8ff30414e915a20efeb0c945222d
George Lima <georgelima11@hotmail.com>
2019-04-05 12:29:23 -0300

    fix: remove trailling space

--- Demo/SelectableText.js
+++ Demo/SelectableText.js
@@ -134,7 +134,7 @@ export const SelectableText = ({
                     }
                   }}
                 >
-                  {text}{' '}
+                  {text}
                 </Text>
               )
             )
