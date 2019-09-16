251145432caf79f070bfcb455c14f334fbb0f9d6
Medson Oliveira <medson.oliveira.jr@gmail.com>
2019-09-16 15:28:11 -0300

    chore(selectable-text): update mapping

--- Demo/SelectableText.js
+++ Demo/SelectableText.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, requireNativeComponent, Platform } from 'react-native'
 import { v4 } from 'uuid'
+import * as R from 'ramda'
 import memoize from 'fast-memoize'
 
 const RNSelectableText = requireNativeComponent('RNSelectableText')
@@ -37,6 +38,8 @@ export class SelectableText extends React.PureComponent {
   render() {
     const { onSelection, onHighlightPress, value, strikenTextStyle, children, ...props } = this.props
 
+
+
     return (
       <RNSelectableText
         {...props}
@@ -45,78 +48,81 @@ export class SelectableText extends React.PureComponent {
         highlights={props.highlights || []}
         ref={this.ref}
       >
-        {value.flatMap(({ id, highlight, text, isStrike }) => {
-          const strikeStyle = isStrike ? strikenTextStyle : {}
-          const highlightStyle = { backgroundColor: props.highlightColor }
+        {R.compose(
+          R.map(({ id, highlight, text, isStrike }) => {
+              const strikeStyle = isStrike ? strikenTextStyle : {}
+              const highlightStyle = { backgroundColor: props.highlightColor }
 
-          const hasHighlights = Array.isArray(highlight) && highlight.length > 0
+              const hasHighlights = Array.isArray(highlight) && highlight.length > 0
 
-          if(!hasHighlights) {
-            return (
-              <Text
-                key={v4()}
-                selectable
-                style={
-                  strikeStyle
+              if(!hasHighlights) {
+                return (
+                  <Text
+                    key={v4()}
+                    selectable
+                    style={
+                      strikeStyle
+                    }
+                  >
+                    {text}
+                  </Text>
+                )
+              }
+
+              const highlightsMapped = highlight.map((item) =>
+                ({
+                  text: text.slice(item.start, item.end + 1),
+                  init: item.start,
+                  end: item.end,
+                  isHighlight: true,
+                })
+              )
+
+              const notHighlightedTexts = highlightsMapped[0].init === 0  && highlightsMapped[0].end === text.length ? [] : highlightsMapped.reduce((acc, item, index) => {
+                const init = index === 0 && item.init !== 0 ?  0 : item.end + 1
+                const end = index < highlightsMapped.length - 1 ? highlightsMapped[index + 1].init - 1 : index === 0 ? item.init - 1 : text.length - 1
+
+                if (index == highlightsMapped.length - 1 && end < text.length - 1) {
+                  const initLastElement = item.end + 1
+                  const endLastElement = text.length
+
+                  return [ ...acc,
+                    {
+                      text: text.slice(init, end + 1),
+                      init,
+                      end,
+                      isHighlight: false,
+                    },
+                    {
+                      text: text.slice(initLastElement, endLastElement),
+                      init: initLastElement,
+                      end: endLastElement,
+                      isHighlight: false,
+                    } ]
                 }
-              >
-                {text}
-              </Text>
-            )
-          }
 
-          const highlightsMapped = highlight.map((item) =>
-            ({
-              text: text.slice(item.start, item.end + 1),
-              init: item.start,
-              end: item.end,
-              isHighlight: true,
-            })
-          )
-
-          const notHighlightedTexts = highlightsMapped[0].init === 0  && highlightsMapped[0].end === text.length ? [] : highlightsMapped.reduce((acc, item, index) => {
-            const init = index === 0 && item.init !== 0 ?  0 : item.end + 1
-            const end = index < highlightsMapped.length - 1 ? highlightsMapped[index + 1].init - 1 : index === 0 ? item.init - 1 : text.length - 1
-
-            if (index == highlightsMapped.length - 1 && end < text.length - 1) {
-              const initLastElement = item.end + 1
-              const endLastElement = text.length
-
-              return [ ...acc,
-                {
+                return [ ...acc, {
                   text: text.slice(init, end + 1),
                   init,
                   end,
                   isHighlight: false,
-                },
-                {
-                  text: text.slice(initLastElement, endLastElement),
-                  init: initLastElement,
-                  end: endLastElement,
-                  isHighlight: false,
                 } ]
-            }
+              }, [])
 
-            return [ ...acc, {
-              text: text.slice(init, end + 1),
-              init,
-              end,
-              isHighlight: false,
-            } ]
-          }, [])
-
-          return highlightsMapped.concat(notHighlightedTexts).sort((a, b) => a.init - b.init).map(item => (
-            <Text
-              key={v4()}
-              selectable
-              style={
-                item.isHighlight ? { ...highlightStyle, ...strikeStyle } : strikeStyle
-              }
-            >
-              {item.text}
-            </Text>
-          ))
-        })}
+              return highlightsMapped.concat(notHighlightedTexts).sort((a, b) => a.init - b.init).map(item => (
+                <Text
+                  key={v4()}
+                  selectable
+                  style={
+                    item.isHighlight ? { ...highlightStyle, ...strikeStyle } : strikeStyle
+                  }
+                >
+                  {item.text}
+                </Text>
+              ))
+}, value),
+          R.flatten,
+        )}
         {props.appendToChildren ? props.appendToChildren : null}
       </RNSelectableText>
     )
